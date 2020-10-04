9d9e3821cf6929c4e99ae847df2eb48685f6c91f
AngadKevin <70760372+AngadKevin@users.noreply.github.com>
2020-10-04 13:23:09 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-var box,boxleftSprite,boxrightSprite,boxBottomBody;
+var side1,side2,side3;
 
 function preload()
 {
@@ -19,7 +19,7 @@ function setup() {
 
 	packageSprite=createSprite(width/2, 80, 10,10);
 	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
+	packageSprite.scale=0.2;
 
 	helicopterSprite=createSprite(width/2, 200, 10,10);
 	helicopterSprite.addImage(helicopterIMG)
@@ -28,67 +28,46 @@ function setup() {
 	groundSprite=createSprite(width/2, height-35, width,10);
 	groundSprite.shapeColor=color(255)
 
+	side1=createSprite(280,610,20,100);
+	side1.shapeColor="red";
+
+	side2=createSprite(380,650,200,20);
+	side2.shapeColor="red";
+
+	side3=createSprite(480,610,20,100);
+	side3.shapeColor="red";
+	
 
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0, isStatic:true});
 	World.add(world, packageBody);
 	
-
 	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
- 	World.add(world, ground);
-
-	 boxPosition= width/2-100
-	 boxY= 610;
-
-	 boxleftSprite= createSprite(boxPosition, boxY, 20, 100);
-	 boxleftSprite.shapeColor=("red");
-
-	
-	 boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100, {isStatic:true} );
-	 World.add(world, boxLeftBody);
-
-
-	 boxBase= createSprite(boxPosition+100, boxY+40, 200, 20);
-	 boxBase.shapeColor=("red");
-
-	 boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20, {isStatic:true} );
-	 World.add(world, boxBottomBody);
-
-	 boxrightSprite= createSprite(boxPosition+200, boxY, 20, 100);
-	 boxrightSprite.shapeColor=("red");
-
-	 boxRightBody = Bodies.rectangle(boxPosition+200-20, boxY, 20,100, {isStatic:true} );
-	 World.add(world, boxRightBody);
+	 World.add(world, ground);
 	 
+
+
 	Engine.run(engine);
-  
-}
+
+	 }
 
 
 function draw() {
   rectMode(CENTER);
   background(0);
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
-
-  
+   keyPressed();
+   packageSprite.x= packageBody.position.x ;
+ packageSprite.y= packageBody.position.y ;
   drawSprites();
-  keyPressed();
-  box.display();
-
-
-  
-}
+ }
 
 function keyPressed() {
- if (keyCode === DOWN_ARROW) {
-    // Look at the hints in the document and understand how to make the package body fall only on
-	
-	Matter.Body.setStatic(packageBody, false);
-  }
+if (keyCode === DOWN_ARROW) {
+Matter.Body.setStatic(packageBody,false);
+}
 }
 
 
